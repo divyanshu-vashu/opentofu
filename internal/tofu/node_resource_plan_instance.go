81c589f15f54f794ed1e73f116213e67c4f72180
Ilia Gogotchuri <ilia.gogotchuri0@gmail.com>
2025-11-14 12:58:58 +0400

    Comment update
    Signed-off-by: Ilia Gogotchuri <ilia.gogotchuri0@gmail.com>

--- internal/tofu/node_resource_plan_instance.go
+++ internal/tofu/node_resource_plan_instance.go
@@ -326,7 +326,7 @@ func (n *NodePlannableResourceInstance) managedResourceExecute(ctx context.Conte
 			// updated value used for further graph execution. However, with
 			// "refresh=false", refreshState is not being written and then
 			// some resources with updated configuration could be detached
-			// due to missaligned create_before_destroy in different graph nodes.
+			// due to missaligned create_before_destroy and skip_destroy in different graph nodes.
 			instanceRefreshState.CreateBeforeDestroy = n.Config.Managed.CreateBeforeDestroy || n.ForceCreateBeforeDestroy
 			instanceRefreshState.SkipDestroy = !n.Config.Managed.Destroy
 
